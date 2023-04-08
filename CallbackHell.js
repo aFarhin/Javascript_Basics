00d1a9d08540cb75fec04b618818ae8df9d16437
Ahmeda Farhin <124598487+aFarhin@users.noreply.github.com>
2023-04-08 19:25:46 +0530

    Update CallbackHell.js

--- CallbackHell.js
+++ CallbackHell.js
@@ -2,17 +2,17 @@
 // 1. The program starts by calling the greet function with the takeOrder function passed as an argument
 // 2. The greet function is executed, and it first prints the string "Say Hello" to the console.
 // 3. Then, it calls the takeOrder function and passes the giveFood function as an argument.
-Then, it calls the takeOrder function and passes the giveFood function as an argument.
+// 4. Then, it calls the takeOrder function and passes the giveFood function as an argument.
 function greet(takeOrder) {
     console.log("Say Hello");
     takeOrder(giveFood);
   }
   
 
-// 4. The takeOrder function is executed and initializes a boolean variable order with the value true. 
-// 5. The function then prints the string "Taking Order...." to the console.
-// 6. Next, it checks if order is true, it prints the string "Your order is true" to the console.
-// 7. If not True, call function thankyou().
+// 5. The takeOrder function is executed and initializes a boolean variable order with the value true. 
+// 6. The function then prints the string "Taking Order...." to the console.
+// 7. Next, it checks if order is true, it prints the string "Your order is true" to the console.
+// 8. If not True, call function thankyou().
   function takeOrder(giveFood) {
     var order = true;
     console.log("Taking Order....");
@@ -25,18 +25,18 @@ function greet(takeOrder) {
   }
   
 
-// 8. The giveFood function is then called with the takePayment function passed as an argument.
-// 9. The giveFood function is executed and prints the string "Giving Food..." to the console.
-// 10.Then, it calls the takePayment function and passes the thankyou function as an argument.
+// 9. The giveFood function is then called with the takePayment function passed as an argument.
+// 10. The giveFood function is executed and prints the string "Giving Food..." to the console.
+// 11.Then, it calls the takePayment function and passes the thankyou function as an argument.
   function giveFood(takePayment) {
     console.log("Giving Food...");
     takePayment(thankyou);
   }
   
 
-// 11.The takePayment function is executed and initializes the payment variable with the string "success".
-// 12.It then prints the string "Please pay for your food" to the console.
-// 13.Since the payment variable is equal to "success", the thankyou function is called.
+// 12.The takePayment function is executed and initializes the payment variable with the string "success".
+// 13.It then prints the string "Please pay for your food" to the console.
+// 14.Since the payment variable is equal to "success", the thankyou function is called.
   function takePayment(thankyou) {
     var payment = "success";
     console.log("Please pay for your food");
@@ -44,13 +44,13 @@ function greet(takeOrder) {
   }
   
 
-// 14.The thankyou function is executed and prints the string "Thanks for coming, please come again" to the console.
+// 15.The thankyou function is executed and prints the string "Thanks for coming, please come again" to the console.
 The program execution is completed.
   function thankyou() {
     console.log("Thanks for coming, please come again");
   }
   
 
-// 15.The program execution is completed.
+// 16.The program execution is completed.
   greet(takeOrder);
   
