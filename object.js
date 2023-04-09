21b50aedeffb3b7473516299e49f2da83809b769
Ahmeda Farhin <124598487+aFarhin@users.noreply.github.com>
2023-04-10 00:35:30 +0530

    Update object.js

--- object.js
+++ object.js
@@ -6,7 +6,8 @@ const farhinArray = [
     ['Neha', 'Ana', 'Maru']
 ]
 
-//so, here is an array of Farhin's details. where some details of Farhin are given like - name, course name, part of course, friends of Farhin. but we can't separately name those detail properties. So, to solve this problem Object is created. where we can write properties and their values together.
+//so, here is an array of Farhin's details. where some details of Farhin are given like - name, course name, part of course, friends of Farhin.
+// but we can't separately name those detail properties. So, to solve this problem Object is created. where we can write properties and their values together.
 
 //key = variable name (properties) 
 // value = any type 
@@ -31,14 +32,17 @@ const farhinObj = {
 //Dot notation ://
 console.log(farhinObj.name);
 
-// value of name property will be showing through the farhinObj object. this dot(.) is an operator which takes the property of the object as "name" and then access the value of this property. output- Farhin
+// value of name property will be showing through the farhinObj object. this dot(.) is an operator which takes the property of the object as "name" 
+// and then access the value of this property. output- Farhin
 
 
 // Bracket Notation ://
 const interestedIn = prompt('What do you want to know about farhin ? choose among name, course, batch, friends');
 console.log(farhinObj[interestedIn]);
 
-// so here if somebody writes course in prompt section, that property will be passed to interestedIn variable. but if we want to show the value of this property as--- console.log(farhinObj.interestedIn)--- it will show error as "Undefined". as interestedIn variable is not declared in the farhinObj object.
+// so here if somebody writes course in prompt section, that property will be passed to interestedIn variable. 
+// but if we want to show the value of this property as--- console.log(farhinObj.interestedIn)--- it will show error as "Undefined". 
+// as interestedIn variable is not declared in the farhinObj object.
 //so to avoid this problem we have to use bracket notation. so that it can take any properties as string through the given variable interesIn. 
 
 if (farhinObj[interestedIn]) {
@@ -81,4 +85,5 @@ console.log(farhinObj);
 // console log this -
 //" Farhin has 3 friends, and her best friend is Neha. Farhin currently is doing a course in Acciojob. She is in frontend batch. She is from arambagh"
 
-console.log(`${farhinObj.name} has ${farhinObj.friends.length} friends, and her best friend is ${farhinObj.friends[0]}. Farhin currently is doing a course in ${farhinObj.course}. She is in ${farhinObj.batch} batch. She is from ${farhinObj.location}`)
+console.log(`${farhinObj.name} has ${farhinObj.friends.length} friends, and her best friend is ${farhinObj.friends[0]}.
+Farhin currently is doing a course in ${farhinObj.course}. She is in ${farhinObj.batch} batch. She is from ${farhinObj.location}`)
